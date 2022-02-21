5fdea6488e3b784ad3140f4413e9caa08643b66b
DevKica <devKica777@gmail.com>
2022-02-21 19:37:20 +0100

    Clean db.ts file
    Remove useless part of middleware (that was just a playground)

--- server/src/prisma/db.ts
+++ server/src/prisma/db.ts
@@ -11,11 +11,7 @@ export const prisma =
   });
 
 prisma.$use(async (params, next) => {
-  console.log("2");
-  console.log(params);
-  console.log(next);
   return next(params);
-  console.log("5");
 });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
