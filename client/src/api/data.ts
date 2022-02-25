8ed72fe77f5eadd76dee2be5775635533110298e
DevKica <devKica777@gmail.com>
2022-02-25 10:07:35 +0100

    📦 NEW: checking the functionality of cookies on client

---
+++ client/src/api/data.ts
@@ -0,0 +1,13 @@
+export const validCreateUserBody = {
+  name: "Pawel",
+  surname: "Kica",
+  email: process.env.REACT_APP_TEST_USER_EMAIL,
+  password: "password1!P",
+  passwordRepetition: "password1!P",
+  birthday: "2003-02-13",
+  longitude: "170.123",
+  latitude: "50.213",
+  showMeGender: "Male",
+  gender: "Male",
+  sexualOrientation: ["Gay", "Lesbian", "Heterosexual"],
+};
---
+++ client/src/api/userInstance.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { validCreateUserBody } from "./data";
+
+axios.defaults.withCredentials = true;
+
+export const userAuthInstance = axios.create({
+  baseURL: "http://localhost:5000/api/v1",
+});
+
+userAuthInstance.interceptors.response.use(
+  res => {
+    console.log(res.data);
+    console.log(res.status);
+    return res;
+  },
+  error => {
+    console.log(error.response);
+    return error;
+  }
+);
+
+export const createUser = () => userAuthInstance.post(`/users`, validCreateUserBody);
